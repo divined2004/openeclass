03f2063e04d486d32d40257b559e9346cfa99cf6
dim <dimkostopoulos@gmail.com>
2014-10-18 15:17:44 +0300

    Exercise 3

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-04 15:42
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = 'toor';
+$mysqlMainDb = 'eclass';
