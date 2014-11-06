0aa44f2552f2cbfc689d7cf3ff0d565d02fc6a6a
divined2004 <divined2004@yahoo.gr>
2014-11-06 10:09:07 +0200

    Changes

--- config/config.php
+++ config/config.php
@@ -1,11 +1,10 @@
 <?php
 /* ========================================================
  * Open eClass 3.0 configuration file
- * Created by install on 2014-10-25 09:28
+ * Created by install on 2014-11-06 09:55
  * ======================================================== */
 
 $mysqlServer = 'localhost';
 $mysqlUser = 'root';
-$mysqlPassword = 'mer450';
+$mysqlPassword = 'bor200';
 $mysqlMainDb = 'eclass';
-$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
